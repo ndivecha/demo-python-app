405a2d2babb4559c054e823329935ac719c2d025
Nitesh Divecha <ndivecha@granitenet.com>
2025-07-15 21:14:02 -0400

    demo: initial commit of demo.
    Signed-off-by: Nitesh Divecha <ndivecha@granitenet.com>

---
+++ demo/hello.py
@@ -0,0 +1,7 @@
+# hello.py
+
+def main():
+    print("Hello world! This is version 1.0.0")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="demo",
+    version="1.0.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "demo = demo.hello:main",
+        ]
+    },
+)
