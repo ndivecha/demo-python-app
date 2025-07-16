aa79f00cee58a1c6aef511908af355f2b5f2979a
Nitesh Divecha <ndivecha@granitenet.com>
2025-07-15 21:25:36 -0400

    demo: bump to ver 1.0.1
    Signed-off-by: Nitesh Divecha <ndivecha@granitenet.com>

--- demo/hello.py
+++ demo/hello.py
@@ -1,7 +1,8 @@
 # hello.py
 
 def main():
-    print("Hello world! This is version 1.0.0")
+    print("Hello world! This is version 1.0.1")
+    print("Why did the my device go to therapy? It had too many unresolved dependencies.")
 
 if __name__ == "__main__":
     main()
